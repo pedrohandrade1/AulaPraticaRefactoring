e24c822083655c8e7ef7deb205d2abb9e54b0d96
carbo6ufmg <carbo6@ufmg.br>
2022-06-25 19:56:04 -0300

    Commit 10

--- Rental.java
+++ Rental.java
@@ -26,9 +26,6 @@ public class Rental {
       //Adicionar o trecho de código extraído.
       // add frequent renter points
       // add bonus for a two day new release rental
-      if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-         this.getDaysRented() > 1) return 2;
-
-      return 1;
+      return _movie.getFrequentRenterPoints(_daysRented);
    }
 }
