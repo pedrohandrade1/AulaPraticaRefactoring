64bbb3c912378b65d011190cc373edb53845759f
carbo6ufmg <carbo6@ufmg.br>
2022-06-25 19:31:05 -0300

    Commit 4

--- Customer.java
+++ Customer.java
@@ -25,8 +25,8 @@ public class Customer {
      while (rentals.hasMoreElements()) {
         double thisAmount = 0;
         Rental each = (Rental) rentals.nextElement();
-        thisAmount = amountFor(each);
-
+        thisAmount = each.getCharge();
+        
         // add frequent renter points
         frequentRenterPoints ++;
         // add bonus for a two day new release rental
@@ -44,26 +44,5 @@ public class Customer {
      result += "You earned " + String.valueOf(frequentRenterPoints) +
              " frequent renter points";
      return result;
-   }
-   private double amountFor(Rental aRental) {
-      //Adicionar o trecho de código extraído.
-      //determine amounts for each line
-      double thisAmount = 0;
-      switch (aRental.getMovie().getPriceCode()) {
-         case Movie.REGULAR:
-            thisAmount += 2;
-            if (aRental.getDaysRented() > 2)
-               thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-            break;
-         case Movie.NEW_RELEASE:
-            thisAmount += aRental.getDaysRented() * 3;
-            break;
-         case Movie.CHILDRENS:
-            thisAmount += 1.5;
-            if (aRental.getDaysRented() > 3)
-               thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-             break;
-      }
-      return thisAmount;
    }
 }
