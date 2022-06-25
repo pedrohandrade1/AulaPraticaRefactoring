5ff87447064d45362336fa37f94238b38690e284
carbo6ufmg <carbo6@ufmg.br>
2022-06-25 19:51:24 -0300

    Commit 9

--- Rental.java
+++ Rental.java
@@ -19,23 +19,8 @@ public class Rental {
    public double getCharge() {
       //Adicionar o trecho de código extraído.
       //determine amounts for each line
-      double thisAmount = 0;
-      switch (this.getMovie().getPriceCode()) {
-         case Movie.REGULAR:
-            thisAmount += 2;
-            if (this.getDaysRented() > 2)
-               thisAmount += (this.getDaysRented() - 2) * 1.5;
-            break;
-         case Movie.NEW_RELEASE:
-            thisAmount += this.getDaysRented() * 3;
-            break;
-         case Movie.CHILDRENS:
-            thisAmount += 1.5;
-            if (this.getDaysRented() > 3)
-               thisAmount += (this.getDaysRented() - 3) * 1.5;
-             break;
-      }
-      return thisAmount;
+
+      return _movie.getCharge(_daysRented);
    }
    public int getFrequentRenterPoints() { 
       //Adicionar o trecho de código extraído.
